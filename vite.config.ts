11e20ef7b14dc6322096391eafc74846b4e14694
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2025-09-02 21:41:10 +0300

    w

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import SitemapPlugin from "vite-plugin-sitemap";
 import { VitePWA } from 'vite-plugin-pwa';
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
-    VitePWA({ 
+    react(),
+    SitemapPlugin({
+      hostname: "https://artbyjulieulfeng.vercel.app/",
+      dynamicRoutes: [
+        "/",
+        "/about",
+        "/blog",
+        "/browse",
+        "/home",
+        "/favourites",
+      ],
+    }),
+    VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
@@ -13,30 +24,30 @@ export default defineConfig({
         short_name: "Julie's Art Gallery",
         theme_color: '#ffffff',
         icons: [
-            {
-                src: 'pwa-64x64.png',
-                sizes: '64x64',
-                type: 'image/png'
-            },
-            {
-                src: 'pwa-192x192.png',
-                sizes: '192x192',
-                type: 'image/png'
-            },
-            {
-                src: 'pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'any'
-            },
-            {
-                src: 'maskable-icon-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable'
-            }
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
         ],
-      }, 
+      },
     })
   ],
 })
